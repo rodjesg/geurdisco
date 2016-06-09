b9aef1fa5cc7a664acbb8864003d2f25a335c841
Rodney Geerlings <rodney@rgeerlings.com>
2016-06-09 14:37:30 +0200

    Verwijderd
    comments

--- index.php
+++ index.php
@@ -5,133 +5,119 @@ require "includes/header.php";
 ?>
 
 
-<div class="content">
-    <div class="container">
-<!--        <!-- Current deals -->-->
-<!--        <div class="row">-->
-<!--            <div class="columns small-12">-->
-<!--                <h3>Current deals</h3>-->
-<!--            </div>-->
-<!--        </div>-->
-<!--        <div class="row owl-carousel deals">-->
-<!--            --><?php
-//            for ($i = 0; $i < 4; $i++) {
-//                include('includes/deals-block.php');
-//            }
-//            ?>
-<!--        </div>-->
-    <!-- Recently added -->
-        <div class="row">
-            <div class="columns small-12">
-                <h3>Recently added</h3>
+    <div class="content">
+        <div class="container">
+            <div class="row">
+                <div class="columns small-12">
+                    <h3>Recently added</h3>
+                </div>
             </div>
-        </div>
-        <div class="row owl-carousel products">
-            <?php
+            <div class="row owl-carousel products">
+                <?php
             for ($i = 0; $i < 8; $i++) {
                 include('includes/product-block.php');
             }
             ?>
-        </div>
-
-    <!-- Ladies -->
-        <div class="row">
-            <div class="columns small-12">
-                <h3>Ladies</h3>
             </div>
-        </div>
-        <div class="row owl-carousel products">
-            <?php
+
+            <!-- Ladies -->
+            <div class="row">
+                <div class="columns small-12">
+                    <h3>Ladies</h3>
+                </div>
+            </div>
+            <div class="row owl-carousel products">
+                <?php
             for ($i = 0; $i < 8; $i++) {
                 include('includes/product-block.php');
             }
             ?>
-        </div>
-        <div class="row">
-            <div class="columns small-12">
-                <a href="#" class="viewmore">View all products <span class="fa fa-chevron-right"></span></a>
             </div>
-        </div>
+            <div class="row">
+                <div class="columns small-12">
+                    <a href="#" class="viewmore">View all products <span class="fa fa-chevron-right"></span></a>
+                </div>
+            </div>
 
-    <!-- Men -->
-        <div class="row">
-            <div class="columns small-12">
-                <h3>Men</h3>
+            <!-- Men -->
+            <div class="row">
+                <div class="columns small-12">
+                    <h3>Men</h3>
+                </div>
             </div>
-        </div>
-        <div class="row owl-carousel products">
-            <?php
+            <div class="row owl-carousel products">
+                <?php
             for ($i = 0; $i < 8; $i++) {
                 include('includes/product-block.php');
             }
             ?>
-        </div>
-        <div class="row">
-            <div class="columns small-12">
-                <a href="#" class="viewmore">View all products <span class="fa fa-chevron-right"></span></a>
             </div>
-        </div>
+            <div class="row">
+                <div class="columns small-12">
+                    <a href="#" class="viewmore">View all products <span class="fa fa-chevron-right"></span></a>
+                </div>
+            </div>
 
-    <!-- Unisex -->
-        <div class="row">
-            <div class="columns small-12">
-                <h3>Unisex</h3>
+            <!-- Unisex -->
+            <div class="row">
+                <div class="columns small-12">
+                    <h3>Unisex</h3>
+                </div>
             </div>
-        </div>
-        <div class="row owl-carousel products">
-            <?php
+            <div class="row owl-carousel products">
+                <?php
             for ($i = 0; $i < 4; $i++) {
                 include('includes/product-block.php');
             }
             ?>
-        </div>
-        <div class="row">
-            <div class="columns small-12">
-                <a href="#" class="viewmore">View all products <span class="fa fa-chevron-right"></span></a>
             </div>
-        </div>
+            <div class="row">
+                <div class="columns small-12">
+                    <a href="#" class="viewmore">View all products <span class="fa fa-chevron-right"></span></a>
+                </div>
+            </div>
 
-    <!-- Children -->
-        <div class="row">
-            <div class="columns small-12">
-                <h3>Children</h3>
+            <!-- Children -->
+            <div class="row">
+                <div class="columns small-12">
+                    <h3>Children</h3>
+                </div>
             </div>
-        </div>
-        <div class="row owl-carousel products">
-            <?php
+            <div class="row owl-carousel products">
+                <?php
             for ($i = 0; $i < 4; $i++) {
                 include('includes/product-block.php');
             }
             ?>
-        </div>
-        <div class="row">
-            <div class="columns small-12">
-                <a href="#" class="viewmore">View all products <span class="fa fa-chevron-right"></span></a>
             </div>
-        </div>
+            <div class="row">
+                <div class="columns small-12">
+                    <a href="#" class="viewmore">View all products <span class="fa fa-chevron-right"></span></a>
+                </div>
+            </div>
 
-    <!-- Special Editions -->
-        <div class="row">
-            <div class="columns small-12">
-                <h3>Special Editions</h3>
+            <!-- Special Editions -->
+            <div class="row">
+                <div class="columns small-12">
+                    <h3>Special Editions</h3>
+                </div>
             </div>
-        </div>
-        <div class="row owl-carousel products">
-            <?php
+            <div class="row owl-carousel products">
+                <?php
             for ($i = 0; $i < 6; $i++) {
                 include('includes/product-block.php');
             }
             ?>
-        </div>
-        <div class="row">
-            <div class="columns small-12">
-                <a href="#" class="viewmore">View all products <span class="fa fa-chevron-right"></span></a>
             </div>
+            <div class="row">
+                <div class="columns small-12">
+                    <a href="#" class="viewmore">View all products <span class="fa fa-chevron-right"></span></a>
+                </div>
+            </div>
+
         </div>
 
-    </div>
 
-
-<?php
+        <?php
 require "includes/footer.php";
 ?>
