b2ee62811cc1629461d5198f3ed95bf4a6b4b523
Rodney Geerlings <rodney@rgeerlings.com>
2016-06-20 23:29:03 +0200

    Google maps

--- pages/winkelzoeker.php
+++ pages/winkelzoeker.php
@@ -5,12 +5,12 @@ $homepath;
 $home = false; 
 require "../includes/header.php";
 ?>
+    <script src="http://maps.googleapis.com/maps/api/js"></script>
 
     <style>
-        .block1Contact {
-            border: 1px solid;
-            border-width: 1px;
-            padding: 12px 20px;
+        #googleMap {
+            margin: auto;
+            padding: auto;
         }
         
         .buttonContact {
@@ -21,15 +21,23 @@ require "../includes/header.php";
 
     </style>
 
-
-
     <div class="content">
         <div class="container">
             <!--  Breadcrums menu   -->
             <?php include "../includes/breadcrumbs.php";?>
+                <script>
+                    function initialize() {
+                        var mapProp = {
+                            center: new google.maps.LatLng(51.583799, 4.796982),
+                            zoom: 15,
+                            mapTypeId: google.maps.MapTypeId.ROADMAP
+                        };
+                        var map = new google.maps.Map(document.getElementById("googleMap"), mapProp);
+                    }
+                    google.maps.event.addDomListener(window, 'load', initialize);
 
-
-
+                </script>
+                <div id="googleMap" style="width:750px;height:380px;"></div>
         </div>
     </div>
 
